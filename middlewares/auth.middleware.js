2562772717083aaf651b4976c4e570bdc66f5cca
Oscar Quiroz <65783932+oscarqochoa@users.noreply.github.com>
2022-06-22 11:52:33 -0500

    Auth

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,15 +4,15 @@ const { verify } = require('jsonwebtoken');
 class AuthMiddleware {
 
     verify = (req, res, next) => {
-
         const token = req.header('auth-token');
+
+        if (!token) return res.status(401).json('Access denied');
 
         const payload = verify(token, 'secret_key');
 
         req.userId = payload._id;
 
         next();
-
     }
 
 }
